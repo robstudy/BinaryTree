72605357acbf514bba0aa891ec5489dd6f3c3866
Robert Garza <robstudent1988@gmail.com>
2018-04-15 13:27:02 -0700

    Added constructor and deconstructor, function deleteAllLeaves

--- BinaryTree.cc
+++ BinaryTree.cc
@@ -3,6 +3,10 @@
 template <class T>
 class BinaryTree {
 	public: 
+		//constructor
+		BinaryTree<T>();
+		//destructor
+		~BinaryTree<T>();
 		void insert(T);
 		void remove(T);
 		bool search(T);
@@ -13,4 +17,24 @@ class BinaryTree {
 		Leaf *left;
 		Leaf *right;
 	};
+	Leaf *root;
+	void deleteAllLeaves(Leaf *leaf);
+	int size;
+};
+
+template <class T>
+BinaryTree<T>::BinaryTree() {
+	size = 0;
+};
+
+template <class T>
+BinaryTree<T>::~BinaryTree() {
+	deleteAllLeaves(root);
+};
+template <class T>
+void BinaryTree<T>::deleteAllLeaves(Leaf *leaf){
+	if(leaf == NULL) return;
+	deleteAllLeaves(leaf->left);
+	deleteAllLeaves(leaf->right);
+	delete leaf;
 };
