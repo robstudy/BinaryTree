f5a86bf883f55334dda35c3569b1ecb7d8ceed26
Robert Garza <robstudent1988@gmail.com>
2018-04-15 13:41:53 -0700

    Added input commands to main.cc to use BinaryTree<int>

--- main.cc
+++ main.cc
@@ -1,6 +1,41 @@
+#include <limits>
 #include "BinaryTree.cc"
 
 int main() {
-	std::cout << "Binary Tree!" << std::endl;
+	std::cout << "Integer Binary Tree!" << std::endl;
+	std::cout << "Instruction!\n" << "Press the following number commands:\n" 
+		<< "1. Insert\n" << "2. Print\n" << "3. Search\n" 
+		<< "4. Remove\n" << "7. Quit\n";
+		
+	BinaryTree<int> bt;
+	char input;
+	int data;
+	while(true) {
+		std::cout << "test\n";
+		std::cin >> input;
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cin.clear();
+		switch(input) {
+			case '1':
+			std::cin >> data;
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cin.clear();
+			break;
+			case '2':
+			
+			break;
+			case '3':
+			
+			break;
+			case '4':
+			
+			break;
+			
+			case '7':
+			exit(EXIT_SUCCESS);
+			default:
+			break;
+		}
+	}
 	return 0;
 }
